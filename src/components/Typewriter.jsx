096cbf5d465c0546cf42d34a7aeaf925b887b564
Dere-Wah <derexcontact@gmail.com>
2024-11-30 12:18:47 +0100

    feat(frontend): add typewriter

---
+++ src/components/Typewriter.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+
+const Typewriter = ({ text, typingSpeed = 100 }) => {
+  const [displayedText, setDisplayedText] = useState("");
+  const [cursorVisible, setCursorVisible] = useState(true);
+
+  useEffect(() => {
+    let index = 0;
+    const intervalId = setInterval(() => {
+      if (index < text.length) {
+        setDisplayedText((prev) => prev + text[index]);
+        index++;
+      } else {
+        clearInterval(intervalId);
+      }
+    }, typingSpeed);
+
+    return () => clearInterval(intervalId);
+  }, [text, typingSpeed]);
+
+  useEffect(() => {
+    const cursorBlinkId = setInterval(() => {
+      setCursorVisible((prev) => !prev);
+    }, 500);
+
+    return () => clearInterval(cursorBlinkId);
+  }, []);
+
+  return (
+    <div style={{ fontFamily: "monospace", whiteSpace: "pre" }}>
+      {displayedText}
+      {cursorVisible ? "_" : " "}
+    </div>
+  );
+};
+
+export default Typewriter;
