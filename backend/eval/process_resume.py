29a0024d88e0c164202aae6b96980bde33a42b2a
Dere-Wah <derexcontact@gmail.com>
2024-12-09 10:28:31 +0100

    fix(backend): actually return invalid roasts

--- backend/eval/process_resume.py
+++ backend/eval/process_resume.py
@@ -3,7 +3,6 @@ from eval.prefrontal_cortex import validate_resume, roast_resume
 from openai import OpenAI
 from models.data_models import ResumeChunks, ResumeResponse
 
-
 with open("./openai.txt") as file:
     key = file.read()
     client = OpenAI(api_key=key)
@@ -11,7 +10,9 @@ with open("./openai.txt") as file:
 def evaluate_resume(resume_text) -> ResumeResponse:
     evaluated = validate_resume(client, resume_text)
     if not evaluated.valid:
-        return ResumeResponse(valid=False, result=ResumeChunks(chunks=[]), invalid_roast=evaluated.invalid_roast)
+        c = ResumeChunks(chunks=[], final_short_honest_overall_consideration="",
+                         one_big_company_that_could_hire_this_profile_and_reason="")
+        return ResumeResponse(valid=False, result=c, invalid_roast=evaluated.invalid_roast)
     else:
         result = roast_resume(client, resume_text)
         return ResumeResponse(valid=True, result=result, invalid_roast="")
