c84a811c5bf9a3965a539b11892c53c0cce07cab
Dere-Wah <derexcontact@gmail.com>
2024-12-10 08:29:43 +0100

    fix(frontend): change invalid roast style

--- frontend/src/components/DisplayResume.jsx
+++ frontend/src/components/DisplayResume.jsx
@@ -6,13 +6,13 @@ import TypewriterList from "./TypewriterList";
 const DisplayResume = ({resume}) => {
     if(!resume.valid){
         return(
-            <div className="w-full h-full bg-slate-100 mb-12 px-12">
+            <div className="w-full bg-slate-100 mb-12 mx-24">
                 <Typewriter text={resume.invalid_roast} />
             </div>
         )
     }else{
         return(
-            <div className="w-full  overflow-hidden overflow-x-clip overflow-y-scroll no-scrollbar mb-12 mx-24">
+            <div className="w-full overflow-hidden overflow-x-clip overflow-y-scroll no-scrollbar mb-12 mx-24">
                 <TypewriterList resume={resume.result} interval={2500}/>
             </div>
         )
